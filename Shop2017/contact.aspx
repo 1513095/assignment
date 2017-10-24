e49d4d6aee8baa17909ee3c13b037baac1d239c0
1513095 <1513095@TSU004MID17438>
2017-10-24 12:13:22 +0100

    contact page cs file changes

--- Shop2017/contact.aspx
+++ Shop2017/contact.aspx
@@ -1,5 +1,6 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/default.Master" AutoEventWireup="true" CodeBehind="contact.aspx.cs" Inherits="Shop2017.contact" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
 </asp:Content>
--- Shop2017/contact.aspx.cs
+++ Shop2017/contact.aspx.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
+using System.Text;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,7 +13,38 @@ namespace Shop2017
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            protected void sendbtn_Click(object sender, EventArgs e)
+            {
+                Page.Validate();
+                if (Page.IsValid)
+                {
 
+                }
+                try
+                {
+
+                    SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+                    smtpClient.UseDefaultCredentials = false;
+                    smtpClient.EnableSsl = true;
+                    smtpClient.Credentials = new System.Net.NetworkCredential("VirtualVolumes@gmail.com", "DontReadThis123");
+                    using (MailMessage message = new MailMessage(totxt.Text, "VirtualVolumes@gmail.com"))
+                    {
+                        message.Subject = subtxt.Text;
+                        StringBuilder sb = new StringBuilder();
+                        sb.AppendLine(nametxt.Text + Environment.NewLine);
+                        sb.AppendLine(messagetxt.Text + Environment.NewLine);
+                        sb.AppendLine(totxt.Text + Environment.NewLine);
+                        message.Body = sb.ToString();
+                        message.IsBodyHtml = false;
+                        smtpClient.Send(message);
+                    }
+                    sendlbl.Text = "Email was sent successfully.";
+                }
+                catch (Exception)
+                {
+                    sendlbl.Text = "Email did not send.";
+                }
+            }
         }
     }
 }
